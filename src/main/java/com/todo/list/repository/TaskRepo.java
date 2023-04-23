76f761215ebf314397d8a42c8e75b06edb3d63d7
S Rakesh Kumar <srk00100@yahoo.com>
2023-04-23 20:17:43 +0530

    a

--- src/main/java/com/todo/list/repository/TaskRepo.java
+++ src/main/java/com/todo/list/repository/TaskRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.todo.list.entity.Task;
+
 @Repository
 public interface TaskRepo extends CrudRepository<Task, Integer > {
     
